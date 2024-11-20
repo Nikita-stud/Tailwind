76e08c431b4c7abec43087f2c4eaf0d8076e0e35
Nikita-stud <nikita151998de@gmail.com>
2024-11-20 17:37:22 +0100

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         'black': '#24242E',
         'blue': '#334D89',
       },
+      shadows: {
+        'round': '1px 2px 2px 0 rgba(0, 0, 0, 0.2)',
+      },
     },
   },
   plugins: [],
