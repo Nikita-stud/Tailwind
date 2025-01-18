8ad12596a65ad17c7135d3caa6f3d300a75d9fec
Nikita-stud <nikita151998de@gmail.com>
2025-01-18 17:45:26 +0100

    Small changes to register

---
+++ js/index.js
@@ -0,0 +1,156 @@
+const API_BASE = "https://v2.api.noroff.dev";
+const API_REGISTER = "/auth/register";
+const API_LOGIN = "/auth/login";
+const API_KEY_URL = "/auth/create-api-key";
+
+// async function getToken(url){
+//   try{
+//     const postData ={
+//       method : `POST`,
+//       // headers: {
+//       //   Authorization: `Bearer ${load("token")}`,
+//       // },
+//       body: JSON.stringify({
+//         name: "My Token JS2 CS",
+//       })
+//     }
+//   const response = await fetch(url, postData);
+//   const json = await response.json();
+//   console.log(json)
+//   return json; 
+
+//   }catch(error){
+//     console.log("Error in registering user")
+//   }
+// }
+// getToken(`${API_BASE}${API_KEY_URL}`);
+
+
+
+
+
+
+// export async function registerUser(url, {name, email, password}){
+//   try{
+//     const postData ={
+//       method : `POST`,
+//       headers: {
+
+//       },
+//       body: JSON.stringify({
+//         "name": name,
+//         "email": email,
+//         "password": password,
+//       })
+//     }
+//   const response = await fetch(url, postData);
+//   const json = await response.json();
+//   return json; 
+
+//   }catch(error){
+//     console.log("Error in registering user")
+//   }
+// }
+// registerUser(`${API_BASE}${API_REGISTER}`, data);
+
+//Must save name, email, password in OBject of data
+
+
+
+
+
+
+
+
+
+
+
+
+// export function loginUser(email, password){
+//   method : `POST`,
+//   header: "",
+//   body: JSON.stringify({
+//     "email": email,
+//     "password": password,
+//   })
+// }
+
+// export function viewPosts(){
+
+// }
+
+// export function filterPosts(){
+
+// }
+
+// export function searchPosts(){
+
+// }
+
+// export function filterSinglePost(){
+
+// }
+
+// export function createPost(){
+
+// }
+
+// export function updatePost(){
+
+// }
+
+// export function deletePost(){
+
+// }
+
+// //Additional features 
+
+// export function commentPost(){
+
+// }
+
+// export function editProfile(){
+
+// }
+
+// export function handleFriends(){
+
+// }
+
+// export function reactToPost(){
+
+// }
+
+const welcomeHeader = document.getElementById("welcome_header");
+const loginContainer = document.getElementById("login_container");
+const createNewText = document.getElementById("create-acc");
+
+function toggleCreateAccount(){
+  createNewText.addEventListener("click", ()=>{
+    loginContainer.classList.toggle("hidden");
+    
+    if(loginContainer.classList.contains("hidden")){
+        welcomeHeader.innerText = "Create Account";
+        createNewText.innerText = "Sign in instead"
+    }else{
+        welcomeHeader.innerText = "Welcome";
+        createNewText.innerText = "Create new account"
+    }
+  })
+}
+toggleCreateAccount()
+
+
+
+
+
+
+
+
+
+// export function handleCLick(event){
+//   const post = {id: event.target.dataset.id};
+//   const favoriteList = JSON.parse(localStorage.get("favorites")) || [];
+//   favoriteList.push(post);
+//   localStorage.setItem("favorites", JSON.stringify(favoriteList));
+// };
