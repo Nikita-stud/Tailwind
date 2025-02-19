b212b7ace06165f04b974b7d9ea21f0cfdf0eff7
Nikita-stud <nikita151998de@gmail.com>
2025-02-19 09:46:31 +0100

    editPosts

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ import { getPosts } from "./api/posts/getPosts.js";
 import { searchPosts } from "./events/posts/searchPosts.mjs";
 import { filterPosts } from "./events/posts/filterPosts.mjs";
 import { deletePost } from "./events/posts/deletePost.mjs";
+import { editPost } from "./events/posts/editPost.mjs";
 
 function pathEvents(){
   const pathName = window.location.pathname;
@@ -30,6 +31,7 @@ function pathEvents(){
             filterPosts(postsObjects)
             searchPosts(postsObjects);
             deletePost();
+            editPost();
           }catch(error){
             console.log(error)
           }
